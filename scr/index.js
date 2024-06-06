54cb37d52c86e732f50ff1c166a08e54a15eaf18
akk2d <daniloparanhos09@gmail.com>
2024-06-05 23:53:14 -0300

    final

--- scr/index.js
+++ scr/index.js
@@ -1,25 +1,29 @@
-let nameXp = ["FyeBwoii", 2351];
+let nameXp = ["FyeBwoii", 10002];
 let classificador = ""
 
 if(nameXp[1] <= 1000){
      classificador = "Ferro"
-}else if(nameXp[1] <= 2000){
+}
+else if(nameXp[1] >= 1001 && nameXp[1] <= 2000){
     classificador = "Bronze"
-}else if (nameXp[1] <= 5000){
+}
+else if (nameXp[1] >= 2001 && nameXp[1] <= 5000){
     classificador = "Prata"
-}else if (nameXp[1] <= 7000){
+}
+else if (nameXp[1] >= 5001 && nameXp[1] <= 7000){
     classificador = "Ouro"
-}else if (nameXp[1] <= 8000){
+}
+else if (nameXp[1] >= 7001 && nameXp[1] <= 8000){
     classificador = "Platina"
-}else if (nameXp[1] <= 9000){
+}
+else if (nameXp[1] >= 8001 && nameXp[1] <= 9000){
     classificador = "Ascendente"
-}else if (nameXp[1] <= 10000){
+}
+else if (nameXp[1] >= 9001 && nameXp[1] <= 10000){
     classificador = "Imortal"
-}else if (nameXp[1] >= 10001){
+}
+else if (nameXp[1] >= 10001){
     classificador = "Radiante"
 }
 
-console.log("O Herói de nome " + nameXp[0] + 
-" está no nível " + classificador + " e tem o total de "
-+ nameXp[1] + "XP")
-
+console.log("O Herói de nome " + nameXp[0] + " está no nível " + classificador + " e tem o total de " + nameXp[1] + "XP")
